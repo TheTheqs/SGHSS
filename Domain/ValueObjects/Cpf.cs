c6c311564c2f16aee8866299d0d23355d28eb445
TheTheqs <matheqs@outlook.com>
2025-11-12 05:16:59 -0300

    CPF (VO) criado e testado. Testes 7/7

---
+++ Domain/ValueObjects/Cpf.cs
@@ -0,0 +1,50 @@
+﻿// Domain/ValueObjects/Cpf.cs
+
+using System.Text.RegularExpressions;
+namespace SGHSS.Domain.ValueObjects
+{
+    public readonly record struct Cpf
+    {
+        private static readonly Regex CpfRegex = new(@"^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$", RegexOptions.Compiled);
+        public string Value { get; }
+
+        public Cpf(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentException("CPF vazio.", nameof(input));
+
+            var d = Regex.Replace(input, @"\D", "");
+
+            if (d.Length != 11)
+                throw new ArgumentException("CPF deve conter 11 dígitos.", nameof(input));
+
+            if (d.All(c => c == d[0]))
+                throw new ArgumentException("CPF inválido (todos os dígitos iguais).", nameof(input));
+
+            if (!CheckDigits(d))
+                throw new ArgumentException("CPF inválido (dígitos verificadores).", nameof(input));
+
+            Value = d;
+        }
+
+        public override string ToString() =>
+            Value.Length == 11
+                ? $"{Value[..3]}.{Value.Substring(3, 3)}.{Value.Substring(6, 3)}-{Value.Substring(9, 2)}"
+                : Value;
+
+        private static bool CheckDigits(string d)
+        {
+            int dv1 = Compute(d.AsSpan(0, 9), 10);
+            int dv2 = Compute(d.AsSpan(0, 10), 11);
+            return (d[9] - '0') == dv1 && (d[10] - '0') == dv2;
+
+            static int Compute(ReadOnlySpan<char> s, int w)
+            {
+                int sum = 0;
+                for (int i = 0; i < s.Length; i++) sum += (s[i] - '0') * w--;
+                int m = sum % 11;
+                return m < 2 ? 0 : 11 - m;
+            }
+        }
+    }
+}
---
+++ Tests/Domain/CpfTests.cs
@@ -0,0 +1,45 @@
+﻿// Tests/Domain/CpfTests.cs
+
+using FluentAssertions;
+using SGHSS.Domain.ValueObjects;
+using Xunit;
+
+namespace SGHSS.Tests.Domain;
+
+public class CpfTests
+{
+    // Um CPF real válido (com DV correto)
+    // 529.982.247-25  (sem máscara: 52998224725). Ambos os formatos devem ser aceitos
+    private const string ValidCpfNoMask = "52998224725";
+    private const string ValidCpfWithMask = "529.982.247-25";
+
+    [Fact]
+    public void Deve_instanciar_com_e_sem_mascara_e_ambos_devem_ser_iguais()
+    {
+        var cpf1 = new Cpf(ValidCpfNoMask);
+        var cpf2 = new Cpf(ValidCpfWithMask);
+
+        cpf1.Value.Should().Be(ValidCpfNoMask);
+        cpf2.Value.Should().Be(ValidCpfNoMask);
+        cpf1.Should().Be(cpf2); // igualdade por valor
+    }
+
+    [Fact]
+    public void ToString_deve_retornar_com_mascara()
+    {
+        var cpf = new Cpf(ValidCpfNoMask);
+        cpf.ToString().Should().Be(ValidCpfWithMask);
+    }
+
+    [Theory]
+    [InlineData("123")]                    // curto
+    [InlineData("529982247251")]           // longo
+    [InlineData("529.982.247-2X")]         // caractere inválido
+    [InlineData("11111111111")]            // repetidos não são válidos
+    [InlineData("52998224724")]            // DV incorreto (último dígito trocado)
+    public void Parse_deve_lancar_para_valores_invalidos(string invalido)
+    {
+        Action act = () => new Cpf(invalido);
+        act.Should().Throw<ArgumentException>();
+    }
+}
