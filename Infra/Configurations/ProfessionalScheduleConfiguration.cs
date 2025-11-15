4a2af91277c0e2ab16bfbda6afff0bfa4d35cdb0
TheTheqs <matheqs@outlook.com>
2025-11-15 16:39:52 -0300

    ProfessionalScheduleConfiguration implementado

---
+++ Infra/Configurations/ProfessionalScheduleConfiguration.cs
@@ -0,0 +1,36 @@
+﻿// Infra/Configurations/ProfessionalScheduleConfiguration.cs
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SGHSS.Domain.Models;
+
+namespace SGHSS.Infra.Configurations
+{
+    /// <summary>
+    /// Provides the Entity Framework Core configuration for the ProfessionalSchedule entity.
+    /// </summary>
+    /// <remarks>
+    /// Defines mappings for relationships between ProfessionalSchedule and
+    /// Professional, SchedulePolicy and ScheduleSlot entries.
+    /// </remarks>
+    public class ProfessionalScheduleConfiguration : IEntityTypeConfiguration<ProfessionalSchedule>
+    {
+        public void Configure(EntityTypeBuilder<ProfessionalSchedule> builder)
+        {
+            builder.ToTable("ProfessionalSchedules");
+
+            builder.HasKey(ps => ps.Id);
+
+            // Relacionamentos
+
+            builder.HasOne(ps => ps.Professional)
+                   .WithOne(p => p.ProfessionalSchedule);
+
+            builder.HasOne(ps => ps.SchedulePolicy)
+                   .WithOne(sp => sp.ProfessionalSchedule);
+
+            builder.HasMany(ps => ps.ScheduleSlots)
+                   .WithOne(ss => ss.ProfessionalSchedule);
+        }
+    }
+}
